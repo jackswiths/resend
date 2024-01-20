f5abcb2097a7a30de51810162add2345f0a9aa85
jackswiths <118581782+jackswiths@users.noreply.github.com>
2024-01-20 09:50:21 +0100

    Update resend.sol

--- resend.sol
+++ resend.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.18;
 // Deployed on Talka for traffic
 // 0x8181DeE11E3E4BF8aDeD436EE499bE7132151bd5
 
+// And Bera also
+// 0x3916cecADbAA33531a5BbdF85f6244Db41FC2315
+
 contract SendBack {
  address payable constant feeAddress = payable(0xD13Cf36b646aDcaD473523F7B32bAa74F4F8F502);
 
